8dd63a8f9b70264a5fb43951599783e7dc1e8409
Yevhenii Huts <yevhenii.huts@globallogic.com>
2024-01-25 23:12:12 +0200

    Initial Commit

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Yevhenii Huts on 25.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
